c68fab63e1468129c2b6609d57c72aa6023ae5ef
dinesdayrit <dayritdines22@gmail.com>
2025-02-02 15:03:34 +0800

    update dashboardLayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -9,10 +9,10 @@ const DashboardLayout = ({ children }: Props) => {
   return (
     <div className="max-h-screen flex flex-col">
       <Header />
-      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto py-4">
         <Sidebar />
-        <div className="overflow-x-hidden px-8 pb-4">
-          <div className="flex justify-center items-center ">{children}</div>
+        <div className="flex justify-center items-center">
+          <div className=" ">{children}</div>
         </div>
       </div>
     </div>
