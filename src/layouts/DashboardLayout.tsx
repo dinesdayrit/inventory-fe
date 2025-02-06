9f01f747a83ab028778495abc390a58cb41a9dd7
dinesdayrit <dayritdines22@gmail.com>
2025-02-06 09:50:09 +0800

    only show menu header on dashboard

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -8,7 +8,7 @@ type Props = {
 const DashboardLayout = ({ children }: Props) => {
   return (
     <div className="max-h-screen flex flex-col">
-      <Header />
+      <Header showMenu />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto py-6">
         <Sidebar />
         <div className="overflow-x-hidden px-10">
