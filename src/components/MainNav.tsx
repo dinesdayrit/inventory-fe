c4b67da7e3be764488d4ca8ce7b026c38a13a1e9
dinesdayrit <dayritdines22@gmail.com>
2025-02-12 13:44:01 +0800

    add dropdown menu on header

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,4 +1,37 @@
+import { Link } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+
 export default function MainNav() {
   const name = localStorage.getItem("currentUser");
-  return <span className="flex space-x-2 items-center">Hello, {name}</span>;
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center px-3  hover:text-violet-500 gap-2">
+          {" "}
+          Hello, {name}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            <Link
+              to="/user-profile"
+              className="font-bold hover:text-violet-500"
+            >
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Button className="flex flex-1 font-bold bg-violet-500">
+              Log out
+            </Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 }
